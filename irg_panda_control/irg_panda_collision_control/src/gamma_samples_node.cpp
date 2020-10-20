c16ae8570c144c1262a2b2f1c7df8eb8b4242857
Nadia Figueroa <nbfigueroa@gmail.com>
2020-10-20 18:03:51 -0400

    Adding stuff on collision control

---
+++ irg_panda_control/irg_panda_collision_control/src/gamma_samples_node.cpp
@@ -0,0 +1,111 @@
+#include <ros/ros.h>
+#include <visualization_msgs/Marker.h>
+#include <cmath>
+#include <vector>
+
+// Linear interpolation following MATLAB linspace
+std::vector<double> LinearSpacedArray(double a, double b, std::size_t N)
+{
+    double h = (b - a) / static_cast<double>(N-1);
+    std::vector<double> xs(N);
+    std::vector<double>::iterator x;
+    double val;
+    for (x = xs.begin(), val = a; x != xs.end(); ++x, val += h) {
+        *x = val;
+    }
+    return xs;
+}
+
+
+int main( int argc, char** argv )
+{
+  ros::init(argc, argv, "spheres");
+  ros::NodeHandle n;
+  ros::Publisher marker_pub = n.advertise<visualization_msgs::Marker>("gamma_samples_3D", 10);
+  ros::Rate r(30);
+
+  double grid_size   = 50;
+  std::vector<double> y_row     = LinearSpacedArray(-0.8, 0.8, grid_size);
+  std::vector<double> x_column  = LinearSpacedArray(0.1, 1.0, grid_size);
+  std::vector<double> z_stacks  = LinearSpacedArray(0.55, 1.1, grid_size);        
+
+  // float f = 0.0;
+  while (ros::ok())
+  {
+
+    visualization_msgs::Marker sphere_list;
+    sphere_list.header.frame_id= "/world";
+    sphere_list.header.stamp= ros::Time::now();
+    sphere_list.ns= "points";
+    sphere_list.action= visualization_msgs::Marker::ADD;
+    sphere_list.pose.orientation.w= 1.0;
+
+    sphere_list.id = 0;
+    sphere_list.type = visualization_msgs::Marker::SPHERE_LIST;
+
+    // POINTS markers use x and y scale for width/height respectively
+    sphere_list.scale.x = 0.01;
+    sphere_list.scale.y = 0.01;
+    sphere_list.scale.z = 0.01;
+    // sphere_list.color.r = 1.0f;
+    // sphere_list.color.a = 0.5f;
+
+
+    for (uint32_t k = 0; k < grid_size; ++k){
+      float z =  z_stacks[k];
+        for (uint32_t i = 0; i < grid_size; ++i){
+          float y =  y_row[i];
+          
+          for (uint32_t j = 0; j < grid_size; ++j){
+            float x =  x_column[j];
+            
+            geometry_msgs::Point p;
+            p.x = x;
+            p.y = y;
+            p.z = z;
+            sphere_list.points.push_back(p);
+
+
+            std_msgs::ColorRGBA c;      
+            // Set all the free space  
+            c.r = 1.0;
+            c.g = 0.0;
+
+            // The table Top
+            if (z > 0.625){
+                c.r = 0.0;
+                c.g = 1.0;              
+            }
+            // The vertical wall
+            if (x>= 0.3){
+              if (y>=-0.02 && y<=0.02){
+                if (z >= 0.625 && z <= 1.025){
+                  c.r = 1.0;
+                  c.g = 0.0;
+                }
+              }
+            }  
+            // The horizontal wall
+            if (x>= 0.3){
+              if (y>=-0.4 && y<=0.4){
+                if (z >= 1.025 && z <= 1.065){
+                  c.r = 1.0;
+                  c.g = 0.0;
+                }
+              }
+            } 
+
+            c.b = 0.0;
+            c.a = 0.05;
+            sphere_list.colors.push_back(c);
+
+            } 
+        }
+    }
+    marker_pub.publish(sphere_list);
+
+    r.sleep();
+
+    // f += 0.04;
+  }
+}
